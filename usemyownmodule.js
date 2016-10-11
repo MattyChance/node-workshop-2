63e6f129de992b16eac0cbbae398a9afa4f73cf7
MattyChance <chenmatty.wang@gmail.com>
2016-10-11 20:41:54 +0000

    added table display to weather forecast

--- usemyownmodule.js
+++ usemyownmodule.js
@@ -1,5 +1,10 @@
 var requestJson = require('./library/request-json.js');
 var prompt = require('prompt');
+var Table = require('cli-table');
+var table = new Table( {
+    colWidths: [50, 30] 
+});
+
 
 var weatherURL = 'https://api.darksky.net/forecast/0e5d36966d4c2aee0a2baa3f22191651/';
 
@@ -23,8 +28,19 @@ function weatherForecast() {
                         if (err) {
                             console.log('An error happened when you request weather data.');
                         } else {
-                            console.log('The current weather in your city is: ' + weatherData.currently.icon);
-                            console.log('In next five days: ' + weatherData.daily.summary);
+                            // console.log('The current weather in your city is: ' + weatherData.currently.icon);
+                            // console.log('In next five days: ' + weatherData.daily.summary);
+                            var date = weatherData.daily.data;
+                            table.push(
+                                { 'currently': weatherData.currently.icon},
+                                { [getDate(date[1].time)]: date[1].icon},
+                                { [getDate(date[2].time)]: date[2].icon},
+                                { [getDate(date[3].time)]: date[3].icon},
+                                { [getDate(date[4].time)]: date[4].icon},
+                                { [getDate(date[5].time)]: date[5].icon}
+                                );
+                                console.log(table.toString());
+                            
                         }
                     })
                 }
@@ -34,3 +50,7 @@ function weatherForecast() {
 }
 
 weatherForecast();
+
+function getDate (time) {
+    return new Date(time * 1000).toString();
+}
